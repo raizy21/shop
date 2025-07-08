e14402255677c4465a63840012079b4619036bcc
Razvan Andrei <chiper_raizy@yahoo.com>
2025-07-08 08:41:37 +0200

    [code] adding the first routes

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.routes.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("app is running...");
-});
+// app.get("/", (req, res) => {
+//   res.send("app is running...");
+// });
+
+app.use("/", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT} -> http://localhost:${PORT}/`);
