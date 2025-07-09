8c1f56d4e1cec4707bb888decfbe455219cf26ed
Razvan Andrei <chiper_raizy@yahoo.com>
2025-07-09 09:48:33 +0200

    [code] adding ejs and first view

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 
@@ -7,6 +8,9 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // app.get("/", (req, res) => {
 //   res.send("app is running...");
