59b465c67e593b10d7088f67d1f4b81a470e2d56
Razvan Andrei <chiper_raizy@yahoo.com>
2025-07-09 10:50:25 +0200

    [code] populating & reandering the first view

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
 // app.get("/", (req, res) => {
 //   res.send("app is running...");
 // });
